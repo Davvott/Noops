224d57860d93e6b40e1384ff096b927594dc8be5
Sassafrass <stephen.davey@my.jcu.edu.au>
2019-06-20 08:53:59 +1000

    Refactor into functions

--- DrumBot/drumbot_disco.py
+++ DrumBot/drumbot_disco.py
@@ -9,34 +9,19 @@ from DrumBot.drumbot import DrumBot
 
 # Initialize DrumBot!
 drumbot = DrumBot()
-print(drumbot)
 
-play(70)  # Testing 1,2,3...
-sleep(0.5)
 
-# Gather relevant globals for Sonic Pi
-samples_list = [key for key in globals().keys()]
-samples = []
-terms = ["DRUM", "ELEC"]
-# print(globals().keys())
-for key in samples_list:
-    for term in terms:
-        if term in key:
-            samples.append(key)
+def main():
+    continue_loop = True
+    choice = "Y"
+    while continue_loop and choice:
+        syncsound()
+        cuesound()
+        input("Enter to continue")
 
-# Normalise naming
-for track in drumbot.tracks:
-    name = str(track.name.upper())
-    # Iterate, randomize choice
-    similar = [s for s in samples if name in s]
-    if not similar:
-        track.name = random.choice(samples)
-    else:
-        track.name = random.choice(similar)
 
 # Sync and Cue threading on Play
 tick = Message()
-
 
 @in_thread
 def syncsound():
@@ -68,8 +53,23 @@ def cuesound():
             sleep(0.25)
 
 
-continue_loop = True
-while continue_loop:
-    syncsound()
-    cuesound()
+def initialize_drumbot(drumbot):
+    # Gather name globals for Sonic Pi
+    samples = []
+    for key in [key for key in globals().keys()]:
+        if "DRUM" in key or "ELEC" in key:
+            samples.append(key)
+    # Normalise naming
+    for track in drumbot.tracks:
+        name = str(track.name.upper())
+        # Iterate, randomize choice
+        similar = [s for s in samples if name in s]
+        if not similar:
+            track.name = random.choice(samples)
+        else:
+            track.name = random.choice(similar)
 
+
+initialize_drumbot(drumbot)
+
+main()
